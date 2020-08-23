71d71721aaccaa1137f64f444af322266fb388e0
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-08-23 18:34:33 +0700

    reactjs

---
+++ trafilc-light/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Trafliclight from "./component/Trafliclight";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Trafliclight />
+    </div>
+  );
+}
+
+export default App;
