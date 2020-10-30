2410a2c81d5ff890dd3e8cbff4021f8430e59dd0
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-10-30 15:19:24 +0700

    Updates

---
+++ precache-manifest.4cd167ac26faa86befa24fdc71f46a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56fd70569e4e85d478a54817e95a654e",
+    "url": "/TodoApp-reactjs/index.html"
+  },
+  {
+    "revision": "f2a321a3b95b5094c1ce",
+    "url": "/TodoApp-reactjs/static/css/main.bc539975.chunk.css"
+  },
+  {
+    "revision": "61abf3a73e1359f91c19",
+    "url": "/TodoApp-reactjs/static/js/2.56365a20.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TodoApp-reactjs/static/js/2.56365a20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a321a3b95b5094c1ce",
+    "url": "/TodoApp-reactjs/static/js/main.6b0bbae9.chunk.js"
+  },
+  {
+    "revision": "de304272b1fbebfc352f",
+    "url": "/TodoApp-reactjs/static/js/runtime-main.affbcd03.js"
+  }
+]);
